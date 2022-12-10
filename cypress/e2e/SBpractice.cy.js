1628713422ff5f8742de588f30af2268c7b8b919
Valerie Ludden <valudden@gmail.com>
2022-12-10 08:10:56 -0700

    Last commmit

--- cypress/e2e/SBpractice.cy.js
+++ cypress/e2e/SBpractice.cy.js
@@ -1,9 +1,4 @@
-// describe('empty spec', () => {
-//   it('passes', () => {
-//     cy.visit('https://example.cypress.io')
-//   })
-// })
-/// <reference types="Cypress" />
+
 context('StagingPractice', function () {
   beforeEach(function(){
     cy.visit('https://stagingbeta.youniqueproducts.com/')
@@ -11,64 +6,153 @@ context('StagingPractice', function () {
 
   });
 
-  it('test', ()=>{
-    let user = {
-        first: "Jane",
-        last: "Doe",
-        email: 'youniquetesting+' + Date.now() + '@outlook.com',
-        password: 'F@kepr3senter',
-        market: 1,
-        street:"3400 W Mayflower Ave",
-        city: "Lehi",
-        state: "Utah",
-        zip: 84043
-    };
-
-  cy.on('uncaught:exception', (err, runnable) => {
-      return false
-  });
-    cy.createAccount(user);
-    cy.visit('https://stagingbeta.youniqueproducts.com/register');
-    // cy.SelectMarket(user);
-    // cy.FillOutPersonalInfo(user);
-    // cy.SelectPresenter(user);
-    // cy.FillOutAddress(user);
-    // cy.PickWebsiteName(user);
-    // cy.AgreeToTerms(user);
-    // cy.SelectBBK(user);
-    // cy.AddPaymentInfo(user);
-    // cy.CompleteRegistration(user);
-
-  })
-
-    // it('test', ()=>{
-    //     let user = {
-    //         first: "Jane",
-    //         last: "Doe",
-    //         email: 'youniquetesting+' + Date.now() + '@outlook.com',
-    //         password: 'F@kepr3senter',
-    //         market: 1,
-    //         street:"3400 W Mayflower Ave",
-    //         city: "Lehi",
-    //         state: "Utah",
-    //         zip: 84043
-    //     };
-    //
-    //     cy.on('uncaught:exception', (err, runnable) => {
-    //         return false
-    //     });
-    //     cy.createAccount(user);
-    //     // cy.visit('https://stagingbeta.youniqueproducts.com/register');
-    //     // cy.SelectMarket(user);
-    //     // cy.FillOutPersonalInfo(user);
-    //     // cy.SelectPresenter(user);
-    //     // cy.FillOutAddress(user);
-    //     // cy.PickWebsiteName(user);
-    //     // cy.AgreeToTerms(user);
-    //     // cy.SelectBBK(user);
-    //     // cy.AddPaymentInfo(user);
-    //     // cy.CompleteRegistration(user);
-    //
-    // })
+  // it('US Test', ()=>{
+  //   let user = {
+  //       first: "Jane",
+  //       last: "Doe",
+  //       email: 'youniquetesting+' + Date.now() + '@outlook.com',
+  //       password: 'F@kepr3senter',
+  //       market: 1,
+  //       street:"3400 W Mayflower Ave",
+  //       city: "Lehi",
+  //       state: "Utah",
+  //       zip: 84043
+  //   };
+  //
+  // cy.on('uncaught:exception', (err, runnable) => {
+  //     return false
+  // });
+  //   cy.createAccount(user);
+  //   cy.visit('https://stagingbeta.youniqueproducts.com/register');
+  //   cy.SelectMarket(user);
+  //   cy.FillOutPersonalInfo(user);
+  //   cy.SelectPresenter(user);
+  //   cy.FillOutAddress(user);
+  //   cy.PickWebsiteName(user);
+  //   cy.AgreeToTerms(user);
+  //   cy.SelectBBK(user);
+  //   cy.AddPaymentInfo(user);
+  //   cy.CompleteRegistration(user);
+  //   cy.visit('https://stagingbeta.youniqueproducts.com');
+  //   cy.Logout();
+  //   cy.Login();
+  // })
+  //
+  //   it('CAN test', ()=>{
+  //       let user = {
+  //           first: "Jane",
+  //           last: "Doe",
+  //           email: 'youniquetesting+' + Date.now() + '@outlook.com',
+  //           password: 'F@kepr3senter',
+  //           market: 2,
+  //           street:"3-1200 Douglas Street",
+  //           city: "Victoria",
+  //           state: "British Columbia",
+  //           zip: 'V8W2E5'
+  //       };
+  //
+  //       cy.on('uncaught:exception', (err, runnable) => {
+  //           return false
+  //       });
+  //       cy.createAccount(user);
+  //       cy.visit('https://stagingbeta.youniqueproducts.com/register');
+  //       cy.SelectMarket(user);
+  //       cy.FillOutPersonalInfo(user);
+  //       cy.SelectPresenter(user);
+  //       cy.FillOutAddress(user);
+  //       cy.PickWebsiteName(user);
+  //       cy.AgreeToTerms(user);
+  //       cy.SelectBBK(user);
+  //       cy.AddPaymentInfo(user);
+  //       cy.CompleteRegistration(user);
+  //
+  //   })
+  //   it('AUS test', ()=>{
+  //       let user = {
+  //           first: "Jane",
+  //           last: "Doe",
+  //           email: 'youniquetesting+' + Date.now() + '@outlook.com',
+  //           password: 'F@kepr3senter',
+  //           market: 3,
+  //           street:"186 Arden St Coogee",
+  //           city: "Coogee",
+  //           state: "Tasmania",
+  //           zip: '2034'
+  //       };
+  //
+  //       cy.on('uncaught:exception', (err, runnable) => {
+  //           return false
+  //       });
+  //       cy.createAccount(user);
+  //       cy.visit('https://stagingbeta.youniqueproducts.com/register');
+  //       cy.SelectMarket(user);
+  //       cy.FillOutPersonalInfo(user);
+  //       cy.SelectPresenter(user);
+  //       cy.FillOutAddress(user);
+  //       cy.PickWebsiteName(user);
+  //       cy.AgreeToTerms(user);
+  //       cy.SelectBBK(user);
+  //       cy.AddPaymentInfo(user);
+  //       cy.CompleteRegistration(user);
+  //
+  //   })
+  //
+  //   it('NZ test', ()=>{
+  //       let user = {
+  //           first: "Jane",
+  //           last: "Doe",
+  //           email: 'youniquetesting+' + Date.now() + '@outlook.com',
+  //           password: 'F@kepr3senter',
+  //           market: 4,
+  //           street:"86 Ure Street",
+  //           city: "Avenal",
+  //           state: "Canterbury",
+  //           zip: '9810'
+  //       };
+  //
+  //       cy.on('uncaught:exception', (err, runnable) => {
+  //           return false
+  //       });
+  //       cy.createAccount(user);
+  //       cy.visit('https://stagingbeta.youniqueproducts.com/register');
+  //       cy.SelectMarket(user);
+  //       cy.FillOutPersonalInfo(user);
+  //       cy.SelectPresenter(user);
+  //       cy.FillOutAddress(user);
+  //       cy.PickWebsiteName(user);
+  //       cy.AgreeToTerms(user);
+  //       cy.SelectBBK(user);
+  //       cy.AddPaymentInfo(user);
+  //       cy.CompleteRegistration(user);
+  //
+  //   })
+  //   it('UK test', ()=>{
+  //       let user = {
+  //           first: "Jane",
+  //           last: "Doe",
+  //           email: 'youniquetesting+' + Date.now() + '@outlook.com',
+  //           password: 'F@kepr3senter',
+  //           market: 5,
+  //           street:"2 Foscote Rise",
+  //           city: "Banbury",
+  //           state: "Oxfordshire",
+  //           zip: 'OX16 9XP'
+  //       };
+  //
+  //       cy.on('uncaught:exception', (err, runnable) => {
+  //           return false
+  //       });
+  //       cy.createAccount(user);
+  //       cy.visit('https://stagingbeta.youniqueproducts.com/register');
+  //       cy.SelectMarket(user);
+  //       cy.FillOutPersonalInfo(user);
+  //       cy.SelectPresenter(user);
+  //       cy.FillOutAddress(user);
+  //       cy.PickWebsiteName(user);
+  //       cy.AgreeToTerms(user);
+  //       cy.SelectBBK(user);
+  //       cy.AddPaymentInfo(user);
+  //       cy.CompleteRegistration(user);
+  //   })
 
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -20,67 +20,92 @@ Cypress.Commands.add('iframe', { prevSubject: 'element' }, $iframe => {
     });
 });
 Cypress.Commands.add('createAccount', (user) =>{
-    // cy.log("************* Create Account *********************");
-    cy.log(user.first);
+    //Navigate to Create Account tab
     cy.get('.desktop-guest-links').click();
     cy.get('#registerTab').click();
-    cy.get('.UserPasswordformError').should('not.exist');
 
-    //verify name errors
-    cy.get('#UserFirstName').click();
-    cy.get('#UserLastName').click();
-    cy.get('#UserEmail').click();
+    // //Verify all inputs are visible
+    cy.get('#UserFirstName').should('be.visible');
+    cy.get('#UserLastName').should('be.visible');
+    cy.get('#UserEmail').should('be.visible');
+    cy.get('#UserPassword').should('be.visible');
+    cy.get('#confirmpassword').should('be.visible');
+    cy.get('#enrollSubmit').click();
 
-    cy.get('.UserFirstNameformError').should('be.visible');
-    cy.get('.UserLastNameformError').should('be.visible');
+    //Verify first name error message appears
 
-    //Fill out name correctly
-    cy.get('#UserFirstName').type(user.first);
+    cy.wait(2000);
+    cy.get('#UserFirstName').clear();
     cy.get('#UserLastName').type(user.last);
-
-    //Get email error
-    cy.get('#UserEmail').click();
-    cy.get('#UserPassword').click();
-    cy.get('.UserEmailformError').should('be.visible');
     cy.get('#UserEmail').type(user.email);
-    cy.get('#enrollSubmit').click();
-
-    //Get password error
-    cy.get('.UserPasswordformError').should('be.visible');
-    cy.get('.confirmpasswordformError').should('be.visible');
-    cy.get('#UserPassword').type('123');
-    cy.get('#confirmpassword').type('123');
-    // cy.get('.confirmpasswordformError').should('be.visible');
-    // cy.get('#UserPassword').clear();
     cy.get('#UserPassword').type(user.password);
-
-    cy.get('#confirmpassword').type('abc');
-    cy.get('.confirmpasswordformError').should('be.visible');
-    cy.get('#UserPassword').clear();
-    cy.get('#UserPassword').type('123');
-    cy.get('.UserPasswordformError').should('be.visible');
-    cy.get('#UserPassword').clear();
-    cy.get('#UserPassword').type(user.password);
-    cy.get('.UserPasswordformError').should('not.exist');
-
-    cy.get('#UserEmail').clear();
-    cy.get('.UserPasswordformError').should('not.exist');
-    cy.get('#UserEmail').type('janedoe');
-
-    cy.get('#enrollSubmit').click();
-    cy.get('.UserEmailformError').should('be.visible');
-    cy.get('#UserEmail').clear();
-    cy.get('#UserEmail').type(user.email);
-    cy.get('#enrollSubmit').click();
-
-
-    //Fill Out Information
-    cy.get('#UserPassword').clear();
-    cy.get('#UserPassword').type(user.password);
-    cy.get('#UserPassword').clear();
     cy.get('#confirmpassword').type(user.password);
     cy.get('#enrollSubmit').click();
+    cy.get('.UserFirstNameformError', {timeout: 5000}).should('be.visible');
 
+    //Verify last name error message appears
+    cy.get('#UserFirstName').type(user.first).click();
+    cy.get('#UserLastName').clear().click();
+    cy.get('#enrollSubmit').click();
+    cy.get('.UserLastNameformError').should('be.visible');
+
+    //Verify email error message appears
+    cy.get('#UserLastName').type(user.last).click();
+    cy.get('#UserEmail').clear().click();
+    cy.get('#enrollSubmit').click();
+    cy.get('.UserEmailformError').should('be.visible');
+
+    //Verify password  error message appears
+    cy.get('#UserEmail').type(user.email);
+    cy.get('#UserPassword').clear().click();
+    cy.get('#enrollSubmit').click();
+    cy.get('.UserPasswordformError').should('be.visible');
+
+
+    //Verify password confirmation error message appears
+    cy.get('#UserPassword').type(user.password);
+    cy.get('#confirmpassword').clear().click();
+    cy.get('#enrollSubmit').click();
+    cy.get('.confirmpasswordformError').should('be.visible');
+
+    //Verify email format without
+    cy.get('#UserEmail').clear().type('janedoe').click();
+    cy.get('#confirmpassword').clear().type(user.password);
+    cy.get('#enrollSubmit').click();
+    cy.get('.UserEmailformError').should('be.visible');
+
+    cy.get('#UserEmail').clear().type('janedoe.com').click();
+    // cy.get('#confirmpassword').type(user.password);
+    cy.get('#enrollSubmit').click();
+    cy.get('.UserEmailformError').should('be.visible');
+
+    cy.get('#UserEmail').clear().type('janedoe@').click();
+    // cy.get('#confirmpassword').type(user.password);
+    cy.get('#enrollSubmit').click();
+    cy.get('.UserEmailformError').should('be.visible');
+
+    //Verify passwords must match
+    cy.get('#UserEmail').clear().type(user.email);
+    cy.get('#UserPassword').clear().type(user.password);
+    cy.get('#confirmpassword').clear().type('123');
+    cy.get('#enrollSubmit').click();
+    cy.get('.confirmpasswordformError').should('be.visible');
+
+    //Fill out form correctly
+    cy.wait(2000);
+    cy.get('#UserFirstName').clear().type(user.first).click();
+    cy.get('#UserLastName').clear().type(user.last).click();
+    cy.get('#UserEmail').clear().type(user.email).click();
+    cy.get('#UserPassword').clear().type(user.password).click();
+    cy.get('#confirmpassword').clear().type(user.password).click();
+
+    cy.wait(3000);
+
+    //Submit
+    cy.get('#enrollSubmit').click();
+
+    //Verify Log in worked
+    cy.get('#desktop-account-links > span').should('contain', user.first)
 });
 
 
@@ -88,26 +113,52 @@ Cypress.Commands.add('createAccount', (user) =>{
 Cypress.Commands.add('navigateToRegisterPage', (user)=> {
     cy.visit('https://stagingbeta.youniqueproducts.com/register')
 });
+
 Cypress.Commands.add('SelectMarket', (user)=> {
-    // this.whereDoYouLiveDropDown(market);
-    cy.get('#countriesFancy', { timeout: 10000 }).should('be.visible');
-    cy.log('*** CLICKED COUNTRIES FANCY **********');
+    cy.get('#countriesFancy', { timeout: 1000 }).should('be.visible');
     cy.get('#countriesFancy').click();
-    cy.get(`#countriesFancy > ul > li:first-child`).click();
-    cy.get("#languageList > li:first-child").click();
-    cy.log('*** ENGLISH **********');
+    cy.log("****** MARKET # ***********");
+    cy.log(user.market);
+    if(user.market === 1){
+        cy.get(`#countriesFancy > ul > li:first-child`).click();
+    }
+    if(user.market === 2){
+        cy.get(`#countriesFancy > ul > li`).eq(1).click();
+    }
+    if(user.market === 3){
+        cy.get(`#countriesFancy > ul > li`).eq(2).click();
+    }
+    if(user.market === 4){
+        cy.get(`#countriesFancy > ul > li`).eq(3).click();
+    }
+    if(user.market === 5){
+        cy.get(`#countriesFancy > ul > li`).eq(4).click();
+    }
+
+    if (user.market < 3) {
+        cy.get("#languageList > li:first-child").click();
+    }
 
 });
 
 Cypress.Commands.add('FillOutPersonalInfo', (user) => {
-
-    cy.get('[name="phone"]').click();
+    cy.wait(5000);
+    cy.get('[name="phone"]', {timeout: 6000}).click();
     cy.get('[name="date_of_birth"]').click();
-    cy.get('div.phoneformError > div.formErrorContent').contains('Phone is required');
+    cy.get('div.phoneformError > div.formErrorContent').should('contain','Phone is required');
     cy.get('[name="phone"]').type('7463334444');
-    cy.get('div.bDateformError > div.formErrorContent').contains('Birthdate is required');
+    cy.get('div.bDateformError > div.formErrorContent').should('contain','Birthdate is required');
     cy.get('[name="date_of_birth"]').click();
     cy.get('[name="date_of_birth"]').type('11111980').type('{enter}');
+    if(user.market === 2){
+        cy.get('#ssn').type('323456789').type('{enter}');
+    }
+    if(user.market === 4){
+        cy.get('#ssn').type('323456789').type('{enter}');
+    }
+    if(user.market === 5){
+        cy.get('#ssn').type('ZR014223A').type('{enter}');
+    }
 
 });
 Cypress.Commands.add('SelectPresenter', (user) => {
@@ -119,15 +170,42 @@ Cypress.Commands.add('SelectPresenter', (user) => {
 });
 Cypress.Commands.add('FillOutAddress', (user) => {
     //if market 1 choose Registration Type
-    cy.get('#step3 > p.row.businessRegistr > span > span:nth-child(1) > label > input').click();
+    if(user.market === 1){
+        cy.get('#step3 > p.row.businessRegistr > span > span:nth-child(1) > label > input').click();
+    }
 
-   //Fill Out Address Information
+    //Verify address is needed
+    cy.get('#continuePaymentBtn > a').click();
+    cy.get('.formErrorContent').should('contain', 'Your address is required');
     cy.get('[name="address1"]').click().type(user.street);
-    cy.get('[name="city"]').click().type(user.city);
-    cy.get('#stateSelector').click();
-    cy.get('[name="state"]').select(user.state);
+
+
+    //Verify zip code is needed
+    cy.get('#continuePaymentBtn > a').click();
+    cy.get('.formErrorContent').should('contain', 'Postal code is required.');
     cy.get('[name="postal_code"]').click().type(user.zip);
-    cy.get('#residential_address_category_id').click();
+
+    //Verify city is needed
+    cy.get('#continuePaymentBtn > a').click();
+    cy.get('.formErrorContent').should('contain', 'City is required.');
+    cy.get('[name="city"]').click().type(user.city);
+
+    //Verify state is needed
+    if(user.market < 3){
+        cy.get('#continuePaymentBtn > a').click();
+        cy.get('.formErrorContent').should('contain', 'State is required');
+        cy.get('#stateSelector').click();
+        cy.get('[name="state"]').select(user.state);
+    }
+    else{
+        cy.get('[name="state"]').select(user.state);
+    }
+
+    cy.get('#continuePaymentBtn > a').click();
+
+    if(user.market === 1){
+        cy.get('#residential_address_category_id').click();
+    }
     cy.get('#continuePaymentBtn > a').click();
 
 
@@ -156,19 +234,28 @@ Cypress.Commands.add('PickWebsiteName', (user) => {
 
 Cypress.Commands.add('AgreeToTerms', (user) => {
     //Agreement 1
-    cy.get('#agree1').click().click();
-    cy.get('.agree1formError').should('be.visible');
-    cy.get('#agree1').click();
+    if(user.market === 1){
+        cy.get('#agree1').click().click();
+        cy.get('.agree1formError').should('be.visible');
+        cy.get('#agree1').click();
+    }
+    else {
+        cy.get('#agree1').click();
+    }
+
     //Agreement 2
     cy.get('#agree2').click();
     cy.get('#agree2').click();
     cy.get('.agree2formError').should('be.visible');
     cy.get('#agree2').click();
    //Agreement 3
-    cy.get('#agree3').click();
-    cy.get('#agree3').click();
-    cy.get('.agree3formError').should('be.visible');
-    cy.get('#agree3').click();
+    if(user.market != 3){
+        cy.get('#agree3').click();
+        cy.get('#agree3').click();
+        cy.get('.agree3formError').should('be.visible');
+        cy.get('#agree3').click();
+    }
+
     //Verify all the error messages have disappeared
     cy.get('.agree1formError').should('not.exist');
     cy.get('.agree2formError').should('not.exist');
@@ -197,8 +284,10 @@ Cypress.Commands.add('SelectBBK', (user) => {
     cy.get('.reactSelect__option').eq(0).should('contain', 'Loyal - clear shimmer').click();
 
     //Select Welcome card
-    cy.get('.pdpItemDropdown').eq(4).click();
-    cy.get('.reactSelect__option').eq(0).should('contain', 'Welcome to Younique! - English').click();
+    if(user.market < 3){
+        cy.get('.pdpItemDropdown').eq(4).click();
+        cy.get('.reactSelect__option').eq(0).should('contain', 'Welcome to Younique! - English').click();
+    }
 
     //Click the continute button
     cy.get('.presenterKitButton').click();
@@ -206,7 +295,9 @@ Cypress.Commands.add('SelectBBK', (user) => {
 
 Cypress.Commands.add('AddPaymentInfo', (user) => {
     //Click on payment section
-    cy.get('.braintree-options-list > div:nth-child(1)').click();
+    if(user.market < 3){
+        cy.get('.braintree-options-list > div:nth-child(1)').click();
+    }
 
     //Enter credit card number
     cy.get('#braintree-hosted-field-number')
@@ -230,17 +321,60 @@ Cypress.Commands.add('AddPaymentInfo', (user) => {
             const $iframeOneBody = $iframe.contents();
             cy.wrap($iframeOneBody)
                 .find('[name="cvv"]')
-                .type('123')
+                .type('123').type('{enter}');
         });
+    if(user.market === 5){
+        cy.wait(6000);
+        cy.get('#Cardinal-CCA-IFrame')
+            .then(($iframe) => {
+                const $iframeOneBody = $iframe.contents();
+                cy.wrap($iframeOneBody)
+                    .find('input.input-field')
+                    .type('1234');
+            });
+        cy.get('#Cardinal-CCA-IFrame')
+            .then(($iframe) => {
+                const $iframeOneBody = $iframe.contents();
+                cy.wrap($iframeOneBody)
+                    .find('[value="SUBMIT"]')
+                    .type('{enter}');
+            });
+        cy.wait(5000);
+        cy.get('#uk_wealth_warning').check();
+    }
 
 });
 
 
 Cypress.Commands.add('CompleteRegistration', (user) => {
-    cy.wait(2000);
+    cy.wait(3000);
     cy.get('#send-it').should('contain', 'Complete My Registration').click();
-    cy.wait(10000)
-   cy.get('#ReceiptPage').should('exist');
+    cy.get('#ReceiptPage', {timeout:15000}).should('exist');
+
+
+});
+
+Cypress.Commands.add('Logout', () => {
+    cy.get('.y_link.y_font1-m').click();
+    cy.get('[data-testid="Mic_Drop"]').click();
+    cy.get('.desktop-guest-links').should('contain', 'Log in');
+});
+
+Cypress.Commands.add('Login', () => {
+    cy.get('.desktop-guest-links').click();
+    cy.get('#login-form').should('be.visible');
+    cy.get('[name="email"]').type('youniquetesting+presenter@outlook.com');
+    cy.get('[name="password"]').type('F@kep');
+    cy.get('[value="LOG IN"]').click();
+    cy.get('#loginError').should('contain','Invalid email or password.');
+    cy.get('[name="email"]').type('youniquetesr@outlook.com');
+    cy.get('[name="password"]').type('F@kepr3senter');
+    cy.get('[value="LOG IN"]').click();
+    cy.get('#loginError').should('contain','Invalid email or password.');
+    cy.get('[name="email"]').type('youniquetesting+presenter@outlook.com');
+    cy.get('[name="password"]').type('F@kepr3senter');
+    cy.get('[value="LOG IN"]').click();
+    cy.get('#desktop-account-links > span').should('contain', 'Pre')
 });
 
 
